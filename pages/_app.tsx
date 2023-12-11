ae205385a728dc4e7953b2dd8afebb24f8ca7b32
RyeinKim <ryein6896@gmail.com>
2023-12-12 00:12:27 +0900

    Keep dev on S700

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from "next/head";
+import Script from "next/script";
+import Header from "@/components/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <>
+          <Head>
+              <meta charSet="UTF-8"/>
+              <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          </Head>
+            <Script
+              strategy="beforeInteractive"
+              src="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.10/dist/sweetalert2.min.js"
+            />
+          <Header />
+        <Component {...pageProps} />
+      </>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+        <Head>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.10/dist/sweetalert2.min.css"/>
+        </Head>
+        <body>
+        <Main/>
+        <NextScript/>
+        </body>
     </Html>
   )
 }
