a18d94fc0f90fc7a02f8f9222c3c4346f7f1c1c9
RyeinKim <ryein6896@gmail.com>
2024-01-12 23:37:14 +0900

    Add HTTP Method: OPTIONS on CORS

--- pages/api/cors.ts
+++ pages/api/cors.ts
@@ -2,7 +2,7 @@ import NextCors from 'nextjs-cors';
 
 export default async function handler(req: any, res: any) {
     await NextCors(req, res, {
-        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
         origin: '*', // 또는 특정 출처만 허용하려면 URL 설정
         optionsSuccessStatus: 200,
     });
